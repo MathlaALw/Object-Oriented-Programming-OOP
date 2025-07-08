49bf335c007b7370bcf6cccdee8d5a96e5673217
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-07-08 07:26:38 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Object_Oriented_Programming_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
